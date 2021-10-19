55a4509f3fc42e7569f5db8b0ec25a17057fe050
Suneth Perera <sunethjoes@gmail.com>
2021-10-19 16:53:25 +1100

    Customer update, customer delete functions

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -47,4 +47,54 @@ class CustomerController extends Controller
             return $this->cpFailureResponse(500, $ex->getMessage());
         }
     }
+
+    public function cpUpdateCustomer(Request $request)
+    {
+        $mValidate = Validator::make($request->all(), [
+            'id' => 'required|integer',
+            'first_name' => 'required|string',
+            'last_name' => 'nullable|string',
+            'email' => 'required|email',
+            'phone' => 'required|string'
+        ]);
+        if ($mValidate->fails()) {
+            return $this->cpFailureResponse(422, $mValidate->errors());
+        }
+        try {
+            $mCustomer = Customer::where('id', $request['id'])->first();
+            $isUpdated = $mCustomer->update([
+                'first_name' => $request['first_name'],
+                'last_name' => $request['last_name'],
+                'email' => $request['email'],
+                'phone' => $request['phone']
+            ]);
+            if ($isUpdated) {
+                $mCustomerUpdated = $mCustomer->refresh();
+                return $this->cpResponseWithResults($mCustomerUpdated, "Customer updated successfully");
+            } else {
+                return $this->cpFailureResponse(500, "Customer could not be updated");
+            }
+        } catch (QueryException $ex) {
+            return $this->cpFailureResponse(500, $ex->getMessage());
+        }
+    }
+
+    public function cpDeleteCustomer(Request $request)
+    {
+        $mValidate = Validator::make($request->all(), [
+            'id' => 'required|integer'
+        ]);
+        if ($mValidate->fails()) {
+            return $this->cpFailureResponse(422, $mValidate->errors());
+        }
+        try {
+            $mDeleted = Customer::where('id', $request['id'])->delete($request->input());
+            if ($mDeleted) {
+                return $this->cpResponseWithResults($mDeleted, "Customer deleted successfully");
+            } else
+                return $this->cpFailureResponse(500, "Could not delete customer.");
+        } catch (QueryException $ex) {
+            return $this->cpFailureResponse(500, $ex->getMessage());
+        }
+    }
 }
